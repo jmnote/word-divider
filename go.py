c5c1c0b24855e0c7a0d0e1218ffb6dc66c3e8457
jmnote <opcore@gmail.com>
2020-05-04 00:10:02 -0400

    .

---
+++ go.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+
+
+rules = [
+['\s+',' '],
+['그곳에서','그 곳에서'],
+]
+
+
+import re
+import sys
+
+if len(sys.argv) != 3:
+  print('Usage:')
+  print("$ python3 go.py 1.txt 2.txt\n")
+  quit()
+
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+f1 = open(filename1, "r")
+f2 = open(filename2, "w")
+
+print( f'=> input file: {filename1}' )
+print( f'=> output file: {filename2}' )
+
+for line in f1:
+  for rule in rules:
+    line = re.sub(rule[0], rule[1], line)
+  print( line )
+  f2.write(line + "\n")
+
+f1.close()
+f2.close()
+
+print( "=> Done...\n" )
+
